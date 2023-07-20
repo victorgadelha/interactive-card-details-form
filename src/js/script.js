d48e94af0947283dd699403ded0c1ff59f1f8da7
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-20 20:15:01 -0300

    feat: setando variáveis de formulário

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
 
+const inputName = document.querySelector('#name')
+const inputNumber = document.querySelector('#number')
+const inputMonth = document.querySelector('#month')
+const inputYear = document.querySelector('#year')
+const inputCVC = document.querySelector('#cvc')
