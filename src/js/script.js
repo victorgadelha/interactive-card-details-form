cee0244735796302df4f70e55c13f6809e35686c
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-20 22:27:44 -0300

    feat: criando validação do input de nome

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,27 @@ const inputNumber = document.querySelector('#number')
 const inputMonth = document.querySelector('#month')
 const inputYear = document.querySelector('#year')
 const inputCVC = document.querySelector('#cvc')
+const formButton = document.querySelector('.form__button')
+
+formButton.addEventListener('click', (e) => {
+    e.preventDefault()
+})
+
+inputName.addEventListener('input', (e) => {
+    validateName(e)
+})
+
+const validateName = (e) => {
+    let name = e.target.value.trim()
+    const isName = /^[a-zA-Z\s]*$/.test(name)
+    if (!isName) {
+        inputName.classList.add('form__invalid')
+    }
+
+    else if (isName || name === '') {
+        inputName.classList.remove('form__invalid')
+    }
+
+}
+
+
